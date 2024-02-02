a9d1138f1c69ad0f728959914f546aa2db6ce55f
Serhat Oral <developer.serhat@hotmail.com>
2024-02-03 01:28:25 +0300

    add basic navbar and sidebar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,5 @@
+<template>
+    
+    <v-app-bar :elevation="8" rounded  title="Application bar" class="flex text-center"></v-app-bar>
+    
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
-  <HelloWorld />
+
+  <v-layout class="rounded rounded-md">
+
+    <NavBar/>
+    
+    <SideBar/>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      <h1>Homepage</h1>
+    </v-main>
+
+  </v-layout>
+  
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+//import HelloWorld from '../components/HelloWorld.vue';
+import NavBar from '@/components/NavBar.vue';
+import SideBar from '@/components/SideBar.vue'
+
+
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    NavBar,
+    SideBar
   },
 });
 </script>
