120a79c6057a57be78e7d16ffee56ad909eb8c57
Serhat Oral <developer.serhat@hotmail.com>
2024-02-08 17:23:17 +0300

    add form validation for signup

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -19,33 +19,38 @@
                 <h5 class="text-center  text-grey ">Create a new account and start discovering new books!</h5>
                 <v-row justify="center">
                     <v-col cols="12" sm="8">
-                        <v-row>
-                            <v-col cols="12" sm="6">
-                                <v-text-field v-model="createUserRequest[0].firstName"
-                                    :error-messages="fieldsError.firstName" label="First Name" outlined dense color="blue"
-                                    autocomplete="false" class="mt-4" />
-                            </v-col>
-                            <v-col cols="12" sm="6">
-                                <v-text-field v-model="createUserRequest[0].lastName" :error-messages="fieldsError.lastName"
-                                    label="Last Name" outlined dense color="blue" autocomplete="false" class="mt-4" />
-                            </v-col>
-                        </v-row>
-                        <v-text-field v-model="createUserRequest[0].email" :error-messages="fieldsError.email" label="Email"
-                            outlined dense color="blue" autocomplete="false" />
-                        <v-text-field v-model="createUserRequest[0].password" :error-messages="fieldsError.password"
-                            label="Password" outlined dense color="blue" autocomplete="false" type="password" />
-                        <v-row>
-                            <v-col cols="12" sm="7">
-                                <v-checkbox label="I Accept AAE" class="mt-n1" color="blue">
-                                </v-checkbox>
-                            </v-col>
-                            <v-col cols="12" sm="5">
-                                <span class="caption blue--text ml-n4">Terms &Conditions</span>
-                            </v-col>
-                        </v-row>
-                        <v-btn color="blue" dark block tile @click="handleSingup">Sign up</v-btn>
+                        <v-form>
+                            <v-row>
+                                <v-col cols="12" sm="6">
+                                    <v-text-field :rules="firstNameRules" v-model="createUserRequest[0].firstName"
+                                        :error-messages="fieldsError.firstName" label="First Name" outlined dense
+                                        color="blue" autocomplete="false" class="mt-4" />
+                                </v-col>
+                                <v-col cols="12" sm="6">
+                                    <v-text-field :rules="lastNameRules" v-model="createUserRequest[0].lastName"
+                                        :error-messages="fieldsError.lastName" label="Last Name" outlined dense color="blue"
+                                        autocomplete="false" class="mt-4" />
+                                </v-col>
+                            </v-row>
+                            <v-text-field :rules="emailRules" v-model="createUserRequest[0].email"
+                                :error-messages="fieldsError.email" label="Email" outlined dense color="blue"
+                                autocomplete="false" />
+                            <v-text-field :rules="passwordRules" v-model="createUserRequest[0].password"
+                                :error-messages="fieldsError.password" label="Password" outlined dense color="blue"
+                                autocomplete="false" type="password" />
+                            <v-row>
+                                <v-col cols="12" sm="7">
+                                    <v-checkbox label="I Accept AAE" class="mt-n1" color="blue">
+                                    </v-checkbox>
+                                </v-col>
+                                <v-col cols="12" sm="5">
+                                    <span class="caption blue--text ml-n4">Terms &Conditions</span>
+                                </v-col>
+                            </v-row>
+                            <v-btn color="blue" :disabled=isButtonDisable dark block tile @click="handleSingup">Sign
+                                up</v-btn>
 
-                        <!-- <h5 class="text-center  grey--text mt-4 mb-3">Or Sign up using</h5>
+                            <!-- <h5 class="text-center  grey--text mt-4 mb-3">Or Sign up using</h5>
                         <div class="d-flex  justify-space-between align-center mx-10 mb-11">
                             <v-btn depressed outlined color="grey">
                                 <v-icon color="red">fab fa-google</v-icon>
@@ -57,6 +62,7 @@
                                 <v-icon color="light-blue lighten-3">fab fa-twitter</v-icon>
                             </v-btn>
                         </div> -->
+                        </v-form>
                     </v-col>
                 </v-row>
             </v-card-text>
@@ -87,6 +93,7 @@ export default {
 
         return {
             snackbar: false,
+            isButtonDisable: true,
             fieldsError: {
                 firstName: null,
                 lastName: null,
@@ -101,7 +108,76 @@ export default {
                     email: '',
                     password: ''
                 }
-            ]
+            ],
+
+            firstNameRules: [
+                value => {
+                    if (value?.length >= 2) {
+
+                        if (this.emailRegex(this.createUserRequest[0].email) && this.createUserRequest[0].lastName.length >= 2
+                            && this.createUserRequest[0].password.length >= 3)  
+                            this.isButtonDisable = false
+
+                        return true
+                    }
+                    this.isButtonDisable = true
+                    return 'password must be more than two characters'
+                },
+            ],
+            lastNameRules: [
+                value => {
+                    if (value?.length >= 2) {
+
+                        if (this.emailRegex(this.createUserRequest[0].email) && this.createUserRequest[0].firstName.length >= 2
+                            && this.createUserRequest[0].password.length >= 3)    
+                            this.isButtonDisable = false
+                        return true
+                    }
+                    this.isButtonDisable = true
+                    return 'last must be more than two characters'
+                },
+            ],
+            emailRules: [
+
+                value => {
+                    if (value) return true
+
+                    this.isButtonDisable = true
+                    return 'You must enter a email.'
+                },
+                value => {
+                    if (value?.length <= 50) return true
+
+                    this.isButtonDisable = true
+                    return 'Name must be less than 50 characters.'
+                },
+                value => {
+                    if (this.emailRegex(value)) {
+
+                        if (this.createUserRequest[0].password.length >= 3 && this.createUserRequest[0].firstName.length >= 2
+                            && this.createUserRequest[0].lastName.length >= 2) 
+                            this.isButtonDisable = false
+
+                        return true
+                    }
+                    this.isButtonDisable = true
+                    return 'E-mail must be valid'
+                },
+            ],
+            passwordRules: [
+                value => {
+                    if (value?.length >= 3) {
+                        
+                        if (this.emailRegex(this.createUserRequest[0].email) && this.createUserRequest[0].firstName.length >= 2
+                            && this.createUserRequest[0].lastName.length >= 2) 
+                            this.isButtonDisable = false
+                        
+                        return true
+                    }
+                    this.isButtonDisable = true
+                    return 'password must be more than three characters'
+                },
+            ],
         }
     },
 
@@ -114,6 +190,13 @@ export default {
             this.$emit('changeStep', this.step);
         },
 
+        emailRegex(email) {
+
+            if (email && /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+                return true
+            }
+            return false
+        },
         handleSingup() {
 
             axios.postData('/customer/create', this.createUserRequest[0])
@@ -162,5 +245,4 @@ export default {
 
 .v-application .rounded-br-xl {
     border-bottom-right-radius: 300px !important;
-}
-</style>
+}</style>
